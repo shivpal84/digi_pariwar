672aa69ff5850043b7f4d8a4bd65479dc896e1ed
shivpal84 <55955591+shivpal84@users.noreply.github.com>
2026-02-24 22:52:52 +0530

    Update lesson_detail_bloc.dart

--- lib/features/lesson/presentation/lesson_detail/bloc/lesson_detail_bloc.dart
+++ lib/features/lesson/presentation/lesson_detail/bloc/lesson_detail_bloc.dart
@@ -5,27 +5,24 @@ import '../../../domain/lesson_repository.dart';
 import 'lesson_detail_event.dart';
 import 'lesson_detail_state.dart';
 
-class LessonDetailBloc
-    extends Bloc<LessonDetailEvent, LessonDetailState> {
-
+class LessonDetailBloc extends Bloc<LessonDetailEvent, LessonDetailState> {
   final LessonsRepository repository;
   final LessonModel lesson;
 
-  LessonDetailBloc(this.repository, this.lesson) : super(LessonDetailInitial()) {
-    on<LoadLessonDetail>(_onLoadLessonDetail);
+  LessonDetailBloc(this.repository, this.lesson)
+      : super(LessonDetailInitial()) {
     on<MarkLessonCompleted>(_onMarkCompleted);
     on<LoadLessonSteps>(_onLoadSteps);
   }
 
   Future<void> _onLoadSteps(
-      LoadLessonSteps event,
-      Emitter<LessonDetailState> emit,
-      ) async {
+    LoadLessonSteps event,
+    Emitter<LessonDetailState> emit,
+  ) async {
     emit(LessonStepsLoading());
 
     try {
-      final steps =
-      await repository.getLessonSteps(event.lessonId);
+      final steps = await repository.getLessonSteps(event.lessonId);
 
       if (steps.isEmpty) {
         emit(LessonDetailError("No steps available"));
@@ -37,38 +34,23 @@ class LessonDetailBloc
     }
   }
 
-
-  void _onLoadLessonDetail(
-      LoadLessonDetail event,
-      Emitter<LessonDetailState> emit,
-      ) {
-    emit(LessonDetailLoaded(event.lesson));
-  }
-
   Future<void> _onMarkCompleted(
-      MarkLessonCompleted event,
-      Emitter<LessonDetailState> emit,
-      ) async {
-    if (state is LessonDetailLoaded) {
-      final lesson = (state as LessonDetailLoaded).lesson;
+    MarkLessonCompleted event,
+    Emitter<LessonDetailState> emit,
+  ) async {
+    if (state is LessonStepsLoaded) {
+      final lesson = (state as LessonStepsLoaded).lesson;
 
       emit(LessonCompletionLoading(lesson));
 
-      // try {
-      //   // 🔄 Simulate API call
-      //   await Future.delayed(const Duration(seconds: 2));
-      //
-      //   final updatedLesson = Lesson(
-      //     id: lesson.id,
-      //     title: lesson.title,
-      //     description: lesson.description,
-      //     status: "completed",
-      //   );
-      //
-      //   emit(LessonCompleted(updatedLesson));
-      // } catch (e) {
-      //   emit(LessonDetailError("Failed to complete lesson"));
-      // }
+      try {
+        // 🔄 Simulate API call
+        await Future.delayed(const Duration(seconds: 2));
+
+        emit(LessonCompleted());
+      } catch (e) {
+        emit(LessonDetailError("Failed to complete lesson"));
+      }
     }
   }
 }
